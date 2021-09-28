098d73e6833defff2efda49cae707070f3fcaf7a
Jason Ryan <jamariryan89@yahoo.com>
2021-09-28 12:52:11 -0500

    Add project files.

---
+++ bootcamp-curriculum/Topic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcamp_curriculum
+{
+    class Topic
+    {
+        private static int NextId { get; set; } = 13;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+        
+        public void Print()
+        {
+            Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}");
+        }
+
+        public Topic()
+        {
+            this.Id = NextId;
+            NextId = NextId + 17;
+        }
+    }
+}
